c8dc5a6a6e2fd5d099ad904c964fb5bb3cdb3b4d
Zain Mehdi <szainmehdi@gmail.com>
2013-10-31 22:20:16 -0700

    More styles

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -6,19 +6,24 @@ require_once($cfg['root_dir'] . "classes/Database.class.php");
 function printSiteHeader($page="home") {
     echo '
     <header>
-        <div id="header-content">
-            <div class="site-logo"><h1 class="title ir">Nauhas.co</h1></div>
-            <nav>
-                <a href="index.php"'; if($page=="home") echo ' class="active"'; echo '>Home</a>
-                <a href="nauhakhans.php"'; if($page=="nauhakhans") echo ' class="active"'; echo '>Nauhakhans</a>
-                <a href="nauhas.php"'; if($page=="nauhas") echo ' class="active"'; echo '>Submit Nauhas</a>
-            </nav>';echo '
+        <div id="header-content-wrap">
+            <div id="header-content">
+                <div class="site-logo"><h1 class="title ir">Nauhas.co</h1></div>
+                <nav>
+                    <a href="index.php"'; if($page=="home") echo ' class="active"'; echo '>Home</a>
+                    <a href="nauhakhans.php"'; if($page=="nauhakhans") echo ' class="active"'; echo '>Nauhakhans</a>
+                    <a href="nauhas.php"'; if($page=="nauhas") echo ' class="active"'; echo '>Submit Nauhas</a>
+                </nav>';echo '
+            </div>
+        </div>
+        <div class="background background-' . $page . '">
+            <div class="background-overlay"></div>
         </div>
     </header>';
 }
 function printSiteFooter() {
     echo '<footer>
             <a href="index.php"><div class="site-logo-footer"></div></a>
-            <p>Copyright &copy; 2013 Z Computers. </p>
+            <p>Copyright &copy; 2013 Z Computers. Designed and developed by <a href="http://google.com/+ZainMehdi" target="_blank" title="S. Zain Mehdi&apos;s Google+">Syed Zain Mehdi</a>.</p>
         </footer>';
 }
--- nauhakhans.php
+++ nauhakhans.php
@@ -19,6 +19,7 @@ require_once($cfg['root_dir'] . "includes/global.inc.php");
 
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/nauhakhans.css">
         <link rel="stylesheet" href="css/fonts.css">
         <script src="js/vendor/modernizr-2.6.2.min.js"></script>
     </head>
@@ -35,12 +36,140 @@ require_once($cfg['root_dir'] . "includes/global.inc.php");
             </div>
             <div class="main-content-wrap">
                 <div class="main-content">
-                    <section id="top_nauhakhans" class="content-section">
-                        <h2><strong>Top</strong> Nauhakhans</h2>
-                        <div class="nauhakhan featured">
-
-                        </div>
-                    </section>
+                    <h2><strong>Top</strong> Nauhakhans</h2>
+                        <section id="top_nauhakhans" class="content-section">
+                            <div class="nauhakhan featured">
+                                <a href="#" class="full-box-link"></a>
+                                <div class="nauhakhan-image" style="background-image: url(http://nauhas.co/images/nauhakhans/nadeem-sarwar/nadeem_sarwar_large.jpg);">
+                                    <div class="nauhakhan-image-overlay"></div>
+                                    <img src="" />
+                                </div>
+                                <div class="nauhakhan-content">
+                                    <span class="nauhakhan-name">Nadeem Sarwar</span>
+                                    <div class="nauhakhan-description-preview">
+                                        <?php
+                                            echo strip_tags('<strong>Syed Nadeem Raza Sarwar Rizvi</strong>&nbsp;is a&nbsp;
+                                            <a href="http://en.wikipedia.org/wiki/Shia" title="Shia">Shia</a>&nbsp;
+                                            <a href="http://en.wikipedia.org/wiki/Pakistan" title="Pakistan">Pakistani</a>
+                                            &nbsp;<a href="http://en.wikipedia.org/wiki/Noha" title="Noha">Noha </a>reciter.
+                                             He is well known in the Shia community and is considered the best Noha Khwaan
+                                             (Noha reciter) of present because of his unique style of recitation.&nbsp;He
+                                             rose to fame in the 1980s when his Nohas gained popularity overseas. Since
+                                             then he has recited several hundred Nohas all over the world wherever a Shia
+                                             population exists. He has also released recorded collections of 7 to 10 Nohas
+                                              every year since 1980 and has published many books of his Nohas.&nbsp;He has
+                                              hitherto released 30 albums of Nauha Cassette and CDs from 1980 - 2012. Though
+                                               he mostly recites in&nbsp;<a href="http://en.wikipedia.org/wiki/Urdu"
+                                               title="Urdu">Urdu</a>, he has also recited in languages such as Sindhi,
+                                               Sariki, Punjabi, Pushto, Pourbi, Hindi, Gujarati, Persian, Arabic, English,
+                                               and French. He is very popular among the youth with many copying his style
+                                               to be similarly popular and some of them have seen success. His collections
+                                               of Nohas are very popular during the Islamic month of&nbsp;
+                                               <a href="http://en.wikipedia.org/wiki/Muharram" title="Muharram">Muharram</a>
+                                               &nbsp;when his Nohas are listened to and recited by many. He has collaborated
+                                                several times with the famous Pakistani Poet&nbsp;
+                                                <a href="http://en.wikipedia.org/wiki/Rehan_Azmi" title="Rehan Azmi">
+                                                Rehan Azmi</a>. Most of the Nohas recited by him are "Kalaam."');
+                                        ?>
+                                        <div class="fade"></div>
+                                    </div>
+                                </div>
+                            </div><!--
+                        ---><div class="nauhakhan featured">
+                                <a href="#" class="full-box-link"></a>
+                                <div class="nauhakhan-image" style="background-image: url(http://nauhas.co/images/nauhakhans/irfan-haider/irfan_haider_large.jpg);">
+                                    <div class="nauhakhan-image-overlay"></div>
+                                    <img src="" />
+                                </div>
+                                <div class="nauhakhan-content">
+                                    <span class="nauhakhan-name">Irfan Haider</span>
+                                    <div class="nauhakhan-description-preview">
+                                        <?php
+                                        echo strip_tags('<p> <strong>Syed Irfan Haider Rizvi</strong>, a prominent emerging
+                                        Nauhakhan in the Shia Pakistani community. He started Nauhakhani at a very early age
+                                         and has gained popularity with his anjuman called Karawan-e-Aza. His nauhas have
+                                         gained a large fanbase within the Pakistani youth. </p>');
+                                        ?>
+                                        <div class="fade"></div>
+                                    </div>
+                                </div>
+                            </div><!--
+                        ---><div class="nauhakhan featured">
+                                <a href="#" class="full-box-link"></a>
+                                <div class="nauhakhan-image" style="background-image: url(img/nauhakhans/nauhakhan_ali_safdar.jpg);">
+                                    <div class="nauhakhan-image-overlay"></div>
+                                    <img src="" />
+                                </div>
+                                <div class="nauhakhan-content">
+                                    <span class="nauhakhan-name">Ali Safdar</span>
+                                    <div class="nauhakhan-description-preview">
+                                        <?php
+                                        echo strip_tags('No description found.');
+                                        ?>
+                                        <div class="fade"></div>
+                                    </div>
+                                </div>
+                            </div><!--
+                        ---><div class="nauhakhan featured">
+                                <a href="#" class="full-box-link"></a>
+                                <div class="nauhakhan-image" style="background-image: url(img/nauhakhans/nauhakhan_farhan_ali.jpg);">
+                                    <div class="nauhakhan-image-overlay"></div>
+                                    <img src="" />
+                                </div>
+                                <div class="nauhakhan-content">
+                                    <span class="nauhakhan-name">Farhan Ali</span>
+                                    <div class="nauhakhan-description-preview">
+                                        <?php
+                                        echo strip_tags('Anjuman Zarb-e-Haideri, Karachi, Pakistan');
+                                        ?>
+                                        <div class="fade"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    <h2><strong>All</strong> Nauhakhans</h2>
+                        <section id="all_nauhakhans" class="content-section">
+                            <div class="nauhakhan small">
+                                <a href="#" class="full-box-link"></a>
+                                <div class="nauhakhan-image" style="background-image: url(http://nauhas.co/images/nauhakhans/nadeem-sarwar/nadeem_sarwar_large.jpg);">
+                                    <div class="nauhakhan-image-overlay"></div>
+                                    <img src="" />
+                                </div><!---
+                            ---><div class="nauhakhan-content">
+                                    <span class="nauhakhan-name">Nadeem Sarwar</span>
+                                </div>
+                            </div><!--
+                        ---><div class="nauhakhan small">
+                                <a href="#" class="full-box-link"></a>
+                                <div class="nauhakhan-image" style="background-image: url(http://nauhas.co/images/nauhakhans/irfan-haider/irfan_haider_large.jpg);">
+                                    <div class="nauhakhan-image-overlay"></div>
+                                    <img src="" />
+                                </div><!---
+                            ---><div class="nauhakhan-content">
+                                    <span class="nauhakhan-name">Irfan Haider</span>
+                                </div>
+                            </div><!--
+                        ---><div class="nauhakhan small">
+                                <a href="#" class="full-box-link"></a>
+                                <div class="nauhakhan-image" style="background-image: url(img/nauhakhans/nauhakhan_ali_safdar.jpg);">
+                                    <div class="nauhakhan-image-overlay"></div>
+                                    <img src="" />
+                                </div><!---
+                            ---><div class="nauhakhan-content">
+                                    <span class="nauhakhan-name">Ali Safdar</span>
+                                </div>
+                            </div><!--
+                        ---><div class="nauhakhan small">
+                                <a href="#" class="full-box-link"></a>
+                                <div class="nauhakhan-image" style="background-image: url(img/nauhakhans/nauhakhan_farhan_ali.jpg);">
+                                    <div class="nauhakhan-image-overlay"></div>
+                                    <img src="" />
+                                </div><!--
+                            ---><div class="nauhakhan-content">
+                                    <span class="nauhakhan-name">Farhan Ali</span>
+                                </div>
+                            </div>
+                        </section>
                 </div>
             </div>
         </div>
